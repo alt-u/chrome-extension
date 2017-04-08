a8fb62bc685dff418b044c7a50f45738f6598061
Ollie Bennett <olliebennett@gmail.com>
2017-04-08 14:39:07 +0100

    Block bad sites when time runs out!

--- js/app.js
+++ js/app.js
@@ -1,83 +1,3 @@
-Settings = function() {
-  this.init();
-  return this;
-}
-
-Settings.prototype = {
-  all: {},
-
-  init: function() {
-    this.load();
-  },
-
-  load: function() {
-    this.all = localStorage['settings'] ? JSON.parse(localStorage['settings']) : {};
-  },
-
-  save: function() {
-    localStorage['settings'] = JSON.stringify(this.all);
-  }
-}
-
-UrlSet = function() {
-  this.init();
-  return this;
-}
-
-UrlSet.prototype = {
-  urls: [],
-  index: 0,
-
-  init: function() {
-    this.load();
-  },
-
-  load: function() {
-    this.urls = localStorage['urls'] ? JSON.parse(localStorage['urls']) : [];
-    this.index = localStorage['index'] ? JSON.parse(localStorage['index']) : 0;
-  },
-
-  save: function() {
-    localStorage['urls'] = JSON.stringify(this.urls); 
-    localStorage['index'] = this.index; 
-  },
-
-  urlExists: function(url) {
-    return (this.urls.indexOf(url) !== -1);
-  },
-
-  create: function(url) {
-    if (!this.urlExists(url)) {
-      url.id = this.index++;
-      this.urls.push(url);
-      this.save();
-    }
-  },
-
-  delete: function(url) {
-    var self = this;
-    this.urls.forEach(function(t, i) {
-      if (t == url) {
-        self.urls.splice(i, 1);
-      }
-    });
-    this.save();
-  }
-}
-
-// blocks access to sites if blocking is enabled
-function shouldBlockSite(tabId, changeInfo, tab) {
-  settings.load();
-  urlSet.load();
-  if (urlSet.urls.indexOf(getHostname(tab.url)) !== -1) {
-    blockSite(tabId);
-  }
-}
-
-// block the page
-function blockSite(tabId) {
-  chrome.tabs.update(tabId, {url: 'blocked.html'});
-}
 
 // register handlers for removing items
 function registerBlockedUrlsRemoveButtons() {
@@ -106,14 +26,8 @@ function toggleBadge(rgbColor, badgeText) {
 }
 
 $(document).ready(function() {
-  urlSet.load();
-  settings.load();
 
   populateBlockedUrls();
-
-  var grace_time = 10; // seconds
-  var deadline = new Date(Date.parse(new Date()) + grace_time * 1000);
-  initializeClock('countdown-clock', deadline);
 
   // handler to add the current site
   $('#block_current_site').click(function() {
@@ -132,62 +46,13 @@ $(document).ready(function() {
 
     window.close();
   });
+
+  $('#fix-everything').click(function() {
+    console.log('Fixing everything!');
+    localStorage['time_remaining'] = 10;
+  });
+
+  // setInterval(function() {
+
+  // }, 1000)
 });
-
-function getTimeRemaining(endtime) {
-  var t = Date.parse(endtime) - Date.parse(new Date());
-  var seconds = Math.floor((t / 1000) % 60);
-  var minutes = Math.floor((t / 1000 / 60) % 60);
-  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-  var days = Math.floor(t / (1000 * 60 * 60 * 24));
-  return {
-    'total': t,
-    'days': days,
-    'hours': hours,
-    'minutes': minutes,
-    'seconds': seconds
-  };
-}
-
-function initializeClock(id, endtime) {
-  var clock = document.getElementById(id);
-
-  if (!clock) {
-    return; // running in background, so no DOM available!
-  }
-
-  var hoursSpan = clock.querySelector('.hours');
-  var minutesSpan = clock.querySelector('.minutes');
-  var secondsSpan = clock.querySelector('.seconds');
-
-  function updateClock() {
-    var t = getTimeRemaining(endtime);
-
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
-    }
-  }
-
-  updateClock();
-  var timeinterval = setInterval(updateClock, 1000);
-}
-
-var getLocation = function(href) {
-    var l = document.createElement("a");
-    l.href = href;
-    return l;
-};
-
-var getHostname = function(href) {
-  var loc = getLocation(href);
-  return loc.hostname;
-}
-
-var settings = new Settings();
-var urlSet = new UrlSet();
-
-chrome.tabs.onUpdated.addListener(shouldBlockSite);
---
+++ js/background.js
@@ -0,0 +1,181 @@
+Settings = function() {
+  this.init();
+  return this;
+}
+
+Settings.prototype = {
+  all: {},
+
+  init: function() {
+    this.load();
+  },
+
+  load: function() {
+    this.all = localStorage['settings'] ? JSON.parse(localStorage['settings']) : {};
+  },
+
+  save: function() {
+    localStorage['settings'] = JSON.stringify(this.all);
+  }
+}
+
+UrlSet = function() {
+  this.init();
+  return this;
+}
+
+UrlSet.prototype = {
+  urls: [],
+  index: 0,
+
+  init: function() {
+    this.load();
+  },
+
+  load: function() {
+    this.urls = localStorage['urls'] ? JSON.parse(localStorage['urls']) : [];
+    this.index = localStorage['index'] ? JSON.parse(localStorage['index']) : 0;
+  },
+
+  save: function() {
+    localStorage['urls'] = JSON.stringify(this.urls); 
+    localStorage['index'] = this.index; 
+  },
+
+  urlExists: function(url) {
+    return (this.urls.indexOf(url) !== -1);
+  },
+
+  create: function(url) {
+    if (!this.urlExists(url)) {
+      url.id = this.index++;
+      this.urls.push(url);
+      this.save();
+    }
+  },
+
+  delete: function(url) {
+    var self = this;
+    this.urls.forEach(function(t, i) {
+      if (t == url) {
+        self.urls.splice(i, 1);
+      }
+    });
+    this.save();
+  }
+}
+
+function urlOnBlacklist(url) {
+  urlSet.load();
+  return urlSet.urls.indexOf(getHostname(url)) !== -1
+}
+
+function processCurrentTab(callback) {
+  chrome.tabs.query(
+    { active: true, windowType: "normal", currentWindow: true },
+    function(d) { callback(d[0].id, null, d[0]); }
+  )
+}
+
+function shouldBlockCurrentSite() {
+  processCurrentTab(shouldBlockSite);
+}
+
+function shouldBlockSite(tabId, changeInfo, tab) {
+  console.log('shouldBlockSite?');
+  console.log("localStorage['time_remaining']", localStorage['time_remaining']);
+  if (!localStorage['time_remaining'] || localStorage['time_remaining'] > 0) {
+    console.log('TIME REMAINS!');
+    return;
+  }
+
+  if (urlOnBlacklist(tab.url)) {
+    blockSite(tabId);
+  }
+}
+
+// block the page
+function blockSite(tabId) {
+  chrome.tabs.update(tabId, {url: 'blocked.html'});
+}
+
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock() {
+  var grace_time = 10; // seconds
+
+  var time_remaining = (localStorage['time_remaining'] == null) ? grace_time : localStorage['time_remaining'];
+
+  var endtime = new Date(Date.parse(new Date()) + time_remaining * 1000);
+
+  var clock = document.getElementById('countdown-clock');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    console.log("t = ", t);
+
+    localStorage['time_remaining'] = t.total / 1000;
+
+    chrome.browserAction.setBadgeText({ text: '' + t.total / 1000 });
+
+    if (clock) {
+      // avoid updating UI if running in background (where no DOM available)!
+      clock.querySelector('.hours').innerHTML = ('0' + t.hours).slice(-2);
+      clock.querySelector('.minutes').innerHTML = ('0' + t.minutes).slice(-2);
+      clock.querySelector('.seconds').innerHTML = ('0' + t.seconds).slice(-2);
+    }
+
+    if (t.total <= 0) {
+      shouldBlockCurrentSite();
+    }
+
+    // Run next iteration if we're still a blacklisted site
+    processCurrentTab(function(tabId, _, tab) {
+      if (urlOnBlacklist(tab.url)) {
+        setTimeout(updateClock, 1000);
+      }
+    });
+  }
+
+  updateClock();
+}
+
+var getLocation = function(href) {
+    var l = document.createElement("a");
+    l.href = href;
+    return l;
+};
+
+var getHostname = function(href) {
+  var loc = getLocation(href);
+  return loc.hostname;
+}
+
+var settings = new Settings();
+var urlSet = new UrlSet();
+
+function tabChanged(tabId, changeInfo, tab) {
+  shouldBlockSite(tabId, changeInfo, tab);
+  if (urlOnBlacklist(tab.url)) {
+    initializeClock();
+  }
+}
+
+chrome.tabs.onUpdated.addListener(tabChanged);
+
+urlSet.load();
+settings.load();
